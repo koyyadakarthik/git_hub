ea0e4dc4b6ee0c2cd9f9c8a9f8c5eac00b9d48d6
koyyadakarthik <koyyadakarthik33@gmail.com>
2022-02-11 18:07:01 +0000

    reg.java

---
+++ reg.java
@@ -0,0 +1,25 @@
+  1  git version
+    2  git --version
+    3  yum install git
+    4  yum update
+    5  ssh-keygen
+    6  ls
+    7  ls
+    8  cd .ssh
+    9  ls
+   10  cat id_rsa.pub
+   11  ls
+   12  git init
+   13  git clone git@github.com:koyyadakarthik/git_hub.git
+   14  ls
+   15  ll
+   16  cd git_hub
+   17  ls
+   18  ls -a
+   19  git config --global user.name 
+   20  git config --global user.name "koyyadakarthik"
+   21  git config --global user.email "koyyadakarthik33@gmail.com"
+   22  git config --list
+   23  ls
+   24  vi reg.java
+
